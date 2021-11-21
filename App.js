5df5ab11f699cec3c572e074d2ee12639f5429d7
jgdml <jgdml2801@gmail.com>
2021-11-21 13:15:28 -0300

    Anotacao model

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Anotacao from './lib/model/anotacao';
 
 export default function App() {
+
+  var a = new Anotacao(1, "teste", "teste", "testes")
+
+  console.warn(a);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
